9a80b8eb7919b9800a9000d0f8ebe61bf43428f6
Philasande Ngubo <142037144+Philasande-Ngubo@users.noreply.github.com>
2023-08-19 18:00:52 +0200

    Add files via upload

---
+++ Login/Main.java
@@ -0,0 +1,12 @@
+/*
+*Login For beginners
+*Author : Philasande Ngubo
+*Date   : 19-August-2023
+*/
+public class Main{
+public static void main(String[] args){
+Login login = new Login();
+}
+
+
+}
